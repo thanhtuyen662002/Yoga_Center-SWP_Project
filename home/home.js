e2e841dfcb74f2d1d0bdb2150d62a585d2e9ec76
dattienle@gmail.com <tiendatle360@gmail.com>
2023-05-23 12:51:58 +0700

    commit home,clb, login, news

---
+++ home/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.news-slider').slick({
+      // dots: true,
+      infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  });
+});
