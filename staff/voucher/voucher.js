2d7e12a235f24803d9b712bfc491d5c7317fec29
dattienle@gmail.com <tiendatle360@gmail.com>
2023-05-30 22:37:30 +0700

    voucher check

---
+++ staff/voucher/voucher.js
@@ -0,0 +1,8 @@
+function showTable() {
+    var table = document.getElementById("contentTable");
+    table.style.display = "table";
+  }
+  function closeTable() {
+    var closeTable = document.getElementById("contentTable");
+    closeTable.style.display = "none";
+  }
