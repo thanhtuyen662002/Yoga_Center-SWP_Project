2f368e30a8af4fbfeb1a03655c691962853a8020
dattienle@gmail.com <tiendatle360@gmail.com>
2023-06-20 20:13:24 +0700

    priceDetail

--- price/priceDetail.js
+++ price/priceDetail.js
@@ -10,3 +10,19 @@ mess.addEventListener("click", function (event) {
     input.value = "";
   }
 });
+
+let qc = document.getElementsByClassName("aa")[0];
+let form = document.getElementsByClassName("bb")[0];
+let submit = document.getElementById("submit");
+let back = document.getElementById("back");
+
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+  qc.style.display = "none";
+  form.style.display = "block";
+});
+back.addEventListener("click", (event) => {
+  event.preventDefault();
+  qc.style.display = "block";
+  form.style.display = "none";
+});
